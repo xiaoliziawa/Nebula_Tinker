b7c15b495c89bd9075bc11429e595cbf6841088c
Eternal-Snowstorm <3512546747@qq.com>
2026-01-09 14:06:37 +0100

    Initializing

---
+++ src/main/java/top/nebula/nebula_tinker/NebulaTinker.java
@@ -0,0 +1,8 @@
+package top.nebula.nebula_tinker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NebulaTinker.MODID)
+public class NebulaTinker {
+	public static final String MODID = "nebula_tinker";
+}
